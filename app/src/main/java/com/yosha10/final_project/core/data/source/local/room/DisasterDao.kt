7dd9db000a33757e89ed1d7df0be3f0a0a31dd6a
JosuaMnl <josuamnalu@gmail.com>
2023-08-12 00:46:00 +0700

    refactor(Dao): Correct query

--- app/src/main/java/com/yosha10/final_project/core/data/source/local/room/DisasterDao.kt
+++ app/src/main/java/com/yosha10/final_project/core/data/source/local/room/DisasterDao.kt
@@ -1,23 +1,25 @@
 package com.yosha10.final_project.core.data.source.local.room
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.yosha10.final_project.core.data.source.local.entity.PropertiesReportEntity
+import com.yosha10.final_project.core.data.source.local.entity.DisasterEntity
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface DisasterDao {
-    @Query("SELECT * FROM properties_report " +
+    @Query("SELECT * FROM disaster " +
             "WHERE (:admin IS NULL OR admin = :admin) " +
             "AND (:disasterType IS NULL OR disaster_type = :disasterType)")
     fun getAllDisaster(
         admin: String? = null,
         disasterType: String? = null
-    ): Flow<List<PropertiesReportEntity>>
+    ): Flow<List<DisasterEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertDisaster(disaster: List<PropertiesReportEntity>)
+    suspend fun insertDisaster(disaster: List<DisasterEntity>)
 
-    @Query("DELETE FROM properties_report")
+    @Query("DELETE FROM disaster")
     suspend fun deleteAllDisaster()
 }
