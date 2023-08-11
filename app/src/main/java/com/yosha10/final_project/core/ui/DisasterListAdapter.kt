16de9e5632c5529e08f1c9278467920497272110
JosuaMnl <josuamnalu@gmail.com>
2023-08-12 01:16:37 +0700

    refactor(DisasterListAdapter): Update paramater data to Disaster due to name changes

--- app/src/main/java/com/yosha10/final_project/core/ui/DisasterListAdapter.kt
+++ app/src/main/java/com/yosha10/final_project/core/ui/DisasterListAdapter.kt
@@ -7,18 +7,17 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.yosha10.final_project.R
-import com.yosha10.final_project.core.domain.model.UrunDayaReport
+import com.yosha10.final_project.core.domain.model.Disaster
 import com.yosha10.final_project.core.utils.DisasterType
 import com.yosha10.final_project.databinding.ItemListDisasterBinding
-import java.util.Locale
 
 class DisasterListAdapter : RecyclerView.Adapter<DisasterListAdapter.DisasterListViewHolder>() {
 
-    private var listData = ArrayList<UrunDayaReport>()
-    var onItemClick: ((UrunDayaReport) -> Unit)? = null
+    private var listData = ArrayList<Disaster>()
+    var onItemClick: ((Disaster) -> Unit)? = null
 
     @SuppressLint("NotifyDataSetChanged")
-    fun setData(newListData: List<UrunDayaReport>?) {
+    fun setData(newListData: List<Disaster>?) {
         if (newListData == null) return
         listData.clear()
         listData.addAll(newListData)
@@ -34,26 +33,24 @@ class DisasterListAdapter : RecyclerView.Adapter<DisasterListAdapter.DisasterLis
     override fun getItemCount(): Int =
         listData.size
 
-
     override fun onBindViewHolder(holder: DisasterListViewHolder, position: Int) {
         holder.bind(listData[position])
     }
 
-
     @Suppress("DEPRECATION")
     inner class DisasterListViewHolder(private val binding: ItemListDisasterBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(data: UrunDayaReport) {
+        fun bind(data: Disaster) {
             with(binding) {
                 itemTvTitle.text = checkIsNull(
-                    data.properties.title,
+                    data.title,
                     itemView.context.getString(R.string.no_title)
                 )
                 itemTvDescription.text = checkIsNull(
-                    data.properties.text,
+                    data.text,
                     itemView.context.getString(R.string.no_description)
                 )
-                val disasterType = data.properties.disaster_type
+                val disasterType = data.disasterType
                 val disasterTypeText = when(DisasterType.valueOf(disasterType.uppercase())) {
                     // the color of the badge card will change based on disaster type
                     DisasterType.FLOOD -> DisasterType.FLOOD.IDvalue
@@ -66,7 +63,7 @@ class DisasterListAdapter : RecyclerView.Adapter<DisasterListAdapter.DisasterLis
                 itemTvDisasterType.text = disasterTypeText
 
                 Glide.with(itemView.context)
-                    .load(data.properties.image_url)
+                    .load(data.imageUrl)
                     .placeholder(R.drawable.placeholder_image)
                     .into(itemIvDisaster)
 
@@ -82,14 +79,11 @@ class DisasterListAdapter : RecyclerView.Adapter<DisasterListAdapter.DisasterLis
                 }
 
                 disasterTypeBadge.setCardBackgroundColor(ContextCompat.getColor(itemView.context, colorBadge))
-
-
             }
         }
 
         private fun checkIsNull(data: String, dataElse: String): String =
             data.ifEmpty { dataElse }
-
 
         init {
             binding.root.setOnClickListener {
@@ -97,5 +91,4 @@ class DisasterListAdapter : RecyclerView.Adapter<DisasterListAdapter.DisasterLis
             }
         }
     }
-
 }
