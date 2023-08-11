6814fafdb92d62e6c4770ae67bb51cb9c196cdfa
JosuaMnl <josuamnalu@gmail.com>
2023-08-12 00:51:41 +0700

    refactor(LocalDataSource): Update entity return value in function due to name changes

--- app/src/main/java/com/yosha10/final_project/core/data/source/local/LocalDataSource.kt
+++ app/src/main/java/com/yosha10/final_project/core/data/source/local/LocalDataSource.kt
@@ -1,6 +1,6 @@
 package com.yosha10.final_project.core.data.source.local
 
-import com.yosha10.final_project.core.data.source.local.entity.PropertiesReportEntity
+import com.yosha10.final_project.core.data.source.local.entity.DisasterEntity
 import com.yosha10.final_project.core.data.source.local.room.DisasterDao
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -11,9 +11,9 @@ class LocalDataSource @Inject constructor(private val disasterDao: DisasterDao) 
     fun getAllDisaster(
         admin: String? = null,
         disasterType: String? = null
-    ): Flow<List<PropertiesReportEntity>> = disasterDao.getAllDisaster(admin, disasterType)
+    ): Flow<List<DisasterEntity>> = disasterDao.getAllDisaster(admin, disasterType)
 
-    suspend fun insertDisaster(disasterList: List<PropertiesReportEntity>) = disasterDao.insertDisaster(disasterList)
+    suspend fun insertDisaster(disasterList: List<DisasterEntity>) = disasterDao.insertDisaster(disasterList)
 
     suspend fun deleteAllDisaster() = disasterDao.deleteAllDisaster()
 }
