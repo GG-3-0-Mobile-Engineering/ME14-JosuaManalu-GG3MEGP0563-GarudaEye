57b4d23da8a271a0105a24b0cbf65363d4122d02
JosuaMnl <josuamnalu@gmail.com>
2023-08-11 16:09:43 +0700

    refactor(ApiService): Change getAllReport function to suspend function

--- app/src/main/java/com/yosha10/final_project/core/data/source/remote/network/ApiService.kt
+++ app/src/main/java/com/yosha10/final_project/core/data/source/remote/network/ApiService.kt
@@ -1,15 +1,14 @@
 package com.yosha10.final_project.core.data.source.remote.network
 
 import com.yosha10.final_project.core.data.source.remote.response.UrunDayaReportResponse
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
     @GET("reports")
-    fun getAllReport(
+    suspend fun getAllReport(
         @Query("admin") admin: String? = null,
         @Query("disaster") disaster: String? = null,
-        @Query("timeperiod") timeperiod: Int? = null,
-    ): Call<UrunDayaReportResponse>
+        @Query("timeperiod") timeperiod: Int = 172_800,
+    ): UrunDayaReportResponse
 }
