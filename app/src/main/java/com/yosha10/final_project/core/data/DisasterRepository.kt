1a24a04a433f6f574bbe4a224c8411a679cb25a7
JosuaMnl <josuamnalu@gmail.com>
2023-08-12 01:11:08 +0700

    refactor(DisasterRepository): Implement NetworkBoundResource code and apply dependency injection

--- app/src/main/java/com/yosha10/final_project/core/data/DisasterRepository.kt
+++ app/src/main/java/com/yosha10/final_project/core/data/DisasterRepository.kt
@@ -1,55 +1,43 @@
 package com.yosha10.final_project.core.data
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import com.yosha10.final_project.core.data.source.local.LocalDataSource
 import com.yosha10.final_project.core.data.source.remote.RemoteDataSource
 import com.yosha10.final_project.core.data.source.remote.network.ApiResponse
-import com.yosha10.final_project.core.domain.model.UrunDayaReport
+import com.yosha10.final_project.core.data.source.remote.response.DisasterGeometriesResponse
+import com.yosha10.final_project.core.domain.model.Disaster
 import com.yosha10.final_project.core.domain.repository.IDisasterRepository
 import com.yosha10.final_project.core.utils.DataMapper
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class DisasterRepository private constructor(
+@Singleton
+class DisasterRepository @Inject constructor(
     private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: LocalDataSource,
 ) : IDisasterRepository {
-
-    companion object {
-        @Volatile
-        private var INSTANCE: DisasterRepository? = null
-
-        fun getInstance(
-            remoteDataSource: RemoteDataSource
-        ): DisasterRepository = INSTANCE ?: synchronized(this) {
-            INSTANCE ?: DisasterRepository(remoteDataSource)
-        }
-    }
-
     override fun getAllReport(
         admin: String?,
-        disaster: String?,
-        timeperiod: Int?
-    ): LiveData<Resource<List<UrunDayaReport>>> {
-        val resultData = MutableLiveData<Resource<List<UrunDayaReport>>>()
-
-        resultData.value = Resource.Loading(null)
-
-        val remoteResponse = remoteDataSource.getAllReport(admin, disaster, timeperiod)
-
-        remoteResponse.observeForever { apiResponse ->
-            when (apiResponse) {
-                is ApiResponse.Success -> {
-                    val report = DataMapper.mapResponseToDomain(apiResponse.data)
-                    resultData.value = Resource.Success(report)
-                }
-
-                is ApiResponse.Empty -> {
-                    resultData.value = Resource.Success(emptyList())
-                }
-
-                is ApiResponse.Error -> {
-                    resultData.value = Resource.Error(apiResponse.errorMessage, null)
-                }
-            }
+        disasterType: String?
+    ): Flow<Resource<List<Disaster>>> =
+    object: NetworkBoundResource<List<Disaster>, List<DisasterGeometriesResponse>>() {
+        override fun loadFromDB(): Flow<List<Disaster>> {
+            return localDataSource.getAllDisaster(admin, disasterType).map { DataMapper.mapEntitiesToDomain(it) }
         }
-        return resultData
-    }
+
+        override suspend fun createCall(): Flow<ApiResponse<List<DisasterGeometriesResponse>>> {
+            return remoteDataSource.getAllReport(admin, disasterType)
+        }
+
+        override suspend fun saveCallResult(data: List<DisasterGeometriesResponse>) {
+            val disasterList = DataMapper.mapResponseToEntities(data)
+//            localDataSource.deleteAllDisaster()
+            localDataSource.insertDisaster(disasterList)
+        }
+
+        override fun shouldFetch(data: List<Disaster>?): Boolean {
+            return data.isNullOrEmpty()
+        }
+    }.asFlow()
 }
