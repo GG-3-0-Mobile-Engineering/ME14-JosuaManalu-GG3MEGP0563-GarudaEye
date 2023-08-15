256ddd180b45cef1fe1bff66bb093a2887517d1a
JosuaMnl <josuamnalu@gmail.com>
2023-08-15 11:33:59 +0700

    test: Add unit tests for DataMapper

--- app/src/main/java/com/yosha10/final_project/core/utils/DataMapper.kt
+++ app/src/main/java/com/yosha10/final_project/core/utils/DataMapper.kt
@@ -2,6 +2,8 @@ package com.yosha10.final_project.core.utils
 
 import com.yosha10.final_project.core.data.source.local.entity.DisasterEntity
 import com.yosha10.final_project.core.data.source.remote.response.DisasterGeometriesResponse
+import com.yosha10.final_project.core.data.source.remote.response.DisasterResponse
+import com.yosha10.final_project.core.data.source.remote.response.DisasterTagsResponse
 import com.yosha10.final_project.core.domain.model.Disaster
 
 object DataMapper {
@@ -41,4 +43,43 @@ object DataMapper {
                 lon = it.lon ?: 0.0,
             )
         }
+
+    fun mapDomainToEntities(input: List<Disaster>): List<DisasterEntity> =
+        input.map {
+            DisasterEntity(
+                pkeyId = it.pkey,
+                createdAt = it.createdAt,
+                status = it.status,
+                imageUrl = it.imageUrl,
+                disasterType = it.disasterType,
+                title = it.title,
+                text = it.text,
+                admin = it.admin,
+                lat = it.lat,
+                lon = it.lon,
+            )
+        }
+
+    fun mapEntitiesToResponse(input: List<DisasterEntity>): List<DisasterGeometriesResponse> {
+        val disasterList = ArrayList<DisasterGeometriesResponse>()
+        input.map {
+            val disaster = DisasterGeometriesResponse(
+                coordinates = listOf(it.lon ?: 0.0, it.lat ?: 0.0),
+                disasterResponse = DisasterResponse(
+                    pkey = it.pkeyId.orEmpty(),
+                    imageUrl = it.imageUrl.orEmpty(),
+                    disasterType = it.disasterType.orEmpty(),
+                    createdAt = it.createdAt.orEmpty(),
+                    title = it.title.orEmpty(),
+                    text = it.text.orEmpty(),
+                    status = it.status.orEmpty(),
+                    disasterTagsResponse = DisasterTagsResponse(
+                        instanceRegionCode = it.admin.orEmpty()
+                    )
+                )
+            )
+            disasterList.add(disaster)
+        }
+        return disasterList
+    }
 }
