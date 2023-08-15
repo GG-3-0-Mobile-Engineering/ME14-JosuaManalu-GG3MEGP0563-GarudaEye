692206e5f4a01351e9b7e7eb4daca41c4eb3b277
JosuaMnl <josuamnalu@gmail.com>
2023-08-15 11:36:32 +0700

    refactor: implement equals, hashCode, and toString in Resource class

--- app/src/main/java/com/yosha10/final_project/core/data/Resource.kt
+++ app/src/main/java/com/yosha10/final_project/core/data/Resource.kt
@@ -6,4 +6,29 @@ sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Loading<T>(data: T? = null) : Resource<T>(data)
 
     class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Resource<*>
+
+        if (data != other.data) return false
+        if (message != other.message) return false
+
+        return true
+    }
+    override fun hashCode(): Int {
+        var result = data?.hashCode() ?: 0
+        result = 31 * result + (message?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString(): String {
+        return when (this) {
+            is Success -> "Success[data=$data]"
+            is Error -> "Error[message=$message]"
+            is Loading -> "Loading"
+        }
+    }
 }
